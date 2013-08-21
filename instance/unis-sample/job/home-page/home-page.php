5fb0df4d86e86e099eb72e9d8cf21f6727beab21
Sergey Poskachey <sergey.poskachey@oxynade.com>
2013-08-21 22:38:56 +0300

    Introduced instance

---
+++ instance/unis-sample/job/home-page/home-page.php
@@ -0,0 +1,21 @@
+<?php
+
+class HomePageJob extends AbstractJob {
+	public function run () {
+		$this->browse($this->spec('base_url'));
+
+		if (!$this->page) {
+			$this->hookTrigger('page-not-found', $this->spec('base_url'));
+		}
+
+		$this->parse();
+
+		$this->extract($this->spec('path_about'));
+
+		if (!$this->results) {
+			$this->hookTrigger('about-section-absent');
+		}
+		
+		$this->storeResults();
+	}
+}
