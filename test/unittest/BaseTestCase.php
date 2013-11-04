9709888ed9629a27c4407b062f7bd14ea80005af
Sergey Poskachey <sergey.poskachey@oxynade.com>
2013-11-04 23:21:18 +0200

    Tweaked unit testing

--- test/unittest/BaseTestCase.php
+++ test/unittest/BaseTestCase.php
@@ -4,6 +4,6 @@ namespace UniScraper\Unittest;
 require_once 'PHPUnit/Autoload.php';
 require_once __DIR__ . '/../../app/autoload.php';;
 
-class Base extends \PHPUnit_Framework_TestCase
+class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
 }
